4fdc914eed9dbb019cd627c6b5aa1139b25bca70
Nekehack <144907468+Nekehack@users.noreply.github.com>
2025-05-21 16:36:10 +0300

    Обновление бота
    Добавление первичных функций, подключение и поиск информации в базе данных пользователей

--- Bot.py
+++ Bot.py
@@ -10,14 +10,16 @@ from io import BytesIO
 import sqlite3
 
 #токен для подключения к боту
-token = '74gY'
+token = '7433058915:AAHj5KtDTJ58OoGGUIayfWpDGOG3v0DnfgY'
 
 #использование токена
 bot = telebot.TeleBot(token)
 
+#Словарь и константы для сохранения состояний
 user_states = {}
-START, LOGIN, PASSWORD, PHOTO = range(4)
+START, LOGIN, PASSWORD,PHOTO = range(4)
 
+#Стартовая часть с проверкой логина и пароля
 @bot.message_handler(commands=['start'])
 def start(message):
     user_states[message.chat.id] = START
@@ -25,160 +27,168 @@ def start(message):
                      text=f"Привет, {message.from_user.first_name}, введи свой логин и пароль, для авторизации!")
     bot.send_message(message.chat.id,text='login:')
 
-
-    # markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    # markup.add('Анализ снимка МРТ', 'Тех.поддержка')
-    # bot.send_message(
-    #     message.chat.id,
-    #     f"Привет, {message.from_user.first_name}! Выбери команду.",
-    #     reply_markup=markup
-    # )
+#Ввод логина
 @bot.message_handler(func=lambda message: user_states.get(message.chat.id) == START)
 def login(message):
     user_states[message.chat.id] = LOGIN
     user_states[f'{message.chat.id}_login'] = message.text
     bot.send_message(message.chat.id, text='password:')
 
-
+#Вводы и проверка пароля
 @bot.message_handler(func=lambda message: user_states.get(message.chat.id) == LOGIN)
 def password(message):
     user_states[message.chat.id] = PASSWORD
-    password = message.text
     user_states[f'{message.chat.id}_password'] = message.text
 
-
+    #Проверка
+    #Незабыть добавить более корректную првоерку
     if user_states[f'{message.chat.id}_login'] == '1' and user_states[f'{message.chat.id}_password'] == '1':
         # bot.send_message(message.chat.id, text=f'Отлично твой логин: '
         #                                        f'{user_states[f"{message.chat.id}_login"]} '
         #                                        f'и пароль {user_states[f"{message.chat.id}_password"]}')
-        role = 'user'
+
+        #Тестовая часть удалить
         print(message.from_user.id)
         print(message.from_user.first_name)
-        print(user_states[f'{message.chat.id}_login'])
-        print(user_states[f'{message.chat.id}_password'])
-        print(role)
+        login = user_states[f'{message.chat.id}_login']
+        password = user_states[f'{message.chat.id}_password']
+
+        #Подключение к базе данных
         conn = sqlite3.connect('bot_base.db')
         cursor = conn.cursor()
 
-        query = """
+        #Запрос
+        query = f"""
         SELECT * FROM users
-        WHERE user_id=815722883
-          AND user_name='Илья'
-          AND login='1'
-          AND password='1'
-          AND role='user'
+        WHERE user_id={message.from_user.id}
+          AND user_name='{message.from_user.first_name}'
+          AND login='{login}'
+          AND password='{password}'
         """
 
-        if cursor.execute(query):
-            bot.send_message(message.chat.id, text='Отлично, ты уже есть в базе!')
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+        #Проверка на наличие в базе
+        if result:
+            bot.send_message(message.chat.id, text='Вход выолнен!')
+
+            query = f"""
+                        SELECT role FROM users
+                        WHERE user_id = {message.chat.id}
+                        AND user_name = '{message.from_user.first_name}'
+                    """
+
+            cursor.execute(query)
+            result = cursor.fetchone()
+
+            print(result)
+            print(result[0] if result else None)
+            role = result[0] if result else None
+
+            bot.send_message(message.chat.id, text=f'Твоя текущая роль: {role}')
+
         else:
             cursor.execute("INSERT INTO users (user_id, user_name, login, password, role) VAlUES (?, ?, ?, ?, ?)",
                            (message.from_user.id,
                             message.from_user.first_name,
                             user_states[f'{message.chat.id}_login'],
                             user_states[f'{message.chat.id}_password'],
-                            role))
+                            'user'))
         work(message)
-    else:
-        bot.send_message(message.chat.id, text='Твой логин или пароль неправельный, попробуй ещё раз')
-        user_states[message.chat.id] = None
-        start(message)
+        conn.commit()
+        conn.close()
 
+    #Если логин или пароль не совпали
+    # else:
+    #     bot.send_message(message.chat.id, text='Твой логин или пароль неправельный, попробуй ещё раз')
+    #     user_states[message.chat.id] = None
+    #     start(message)
+
+
+# функции кнопок
 @bot.message_handler(content_types=['text'])
 def work(message):
-    pass
+    print('ЗДесь работает')
+    # Создаем соединение с базой данных
+    conn = sqlite3.connect('bot_base.db')
+    cursor = conn.cursor()
 
+    # Выполняем запрос для определения роли пользователя
+    query = f"""
+            SELECT role FROM users
+            WHERE user_id = {message.chat.id}
+            AND user_name = '{message.from_user.first_name}'
+        """
 
-# @bot.message_handler(content_types=['text'])
-# def
+    cursor.execute(query)
+    result = cursor.fetchone()
+    role = result[0] if result else None
 
-# @bot.message_handler(func=lambda m: True)
-# def handle_message(message):
-#     user_id = message.from_user.id
-#     text = message.text
-#
-#     if text == 'Анализ снимка МРТ':
-#         user_states[user_id] = 'waiting_for_image'
-#         bot.send_message(message.chat.id, "Пожалуйста, отправьте изображение МРТ для анализа.")
-#         return
-#     elif text == 'Тех.поддержка':
-#         bot.send_message(message.chat.id, "Техподдержка в разработке.")
-#         return
-#     else:
-#         # Если ожидаем фото, но пришел текст
-#         if user_states.get(user_id) == 'waiting_for_image':
-#             bot.send_message(message.chat.id, "Пожалуйста, отправьте изображение для анализа.")
-#         else:
-#             bot.send_message(message.chat.id, "Я вас понял. Выберите команду.")
-#
-# # Обработчик для фото
-# @bot.message_handler(content_types=['photo'])
-# def handle_photo(message):
-#     user_id = message.from_user.id
-#     if user_states.get(user_id) == 'waiting_for_image':
-#         try:
-#             # Получаем файл фото
-#             file_id = message.photo[-1].file_id
-#             file_info = bot.get_file(file_id)
-#             downloaded_file = bot.download_file(file_info.file_path)
-#
-#             # Создаем BytesIO объект
-#             image_stream = BytesIO(downloaded_file)
-#
-#             # Анализируем изображение
-#             bot.send_message(message.chat.id, "Обрабатываю изображение, пожалуйста, подождите...")
-#             mrt_analis(image_stream, message)
-#
-#             # После обработки сбрасываем состояние
-#             user_states[user_id] = None
-#         except Exception as e:
-#             bot.send_message(message.chat.id, f"Ошибка при обработке изображения: {e}")
-#     else:
-#         bot.send_message(message.chat.id, "Пожалуйста, сначала выберите команду для анализа.")
-#
-# def mrt_analis(image_stream, message):
-#     model_path = '/Users/ilia/DeiT/DeiT_weights.pth'
-#     # Загружаем модель
-#     try:
-#         # Используйте вашу собственную реализацию DeiT
-#         model = DeiT(
-#             in_channels=3,
-#             patch_size=16,
-#             emb_size=384,  # важно совпадение с тем, что было при обучении
-#             img_size=224,
-#             depth=12,
-#             n_classes=2
-#         )
-#         model.load_state_dict(torch.load(model_path, map_location='cpu'))
-#         model.eval()
-#
-#
-#     except Exception as e:
-#         # bot.send_message(message.chat.id, f"Ошибка загрузки модели: {e}")
-#         print(e)
-#         return
-#
-#     # Трансформы
-#     transform = transforms.Compose([
-#         transforms.Resize((150, 150)),
-#         transforms.ToTensor(),
-#         transforms.Normalize([0.485, 0.456, 0.406],
-#                              [0.229, 0.224, 0.225])
-#     ])
-#
-#     # Предсказание
-#     def predict_image(image_stream):
-#         image = Image.open(image_stream).convert('RGB')
-#         image = transform(image)
-#         image = image.unsqueeze(0)
-#         with torch.no_grad():
-#             output = model(image)
-#         return output
-#
-#     predictions = predict_image(image_stream)
-#     # Тут можно обработать predictions и отправить результат
-#     bot.send_message(message.chat.id, f"Результат анализа: {str(predictions)}")
-#     print(predictions)
-#     print(1)
+    markup = types.InlineKeyboardMarkup()
+    markup = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton("Анализ изображения", callback_data='analyze_image')
+    print("И тут")
+    if role == 'user':
+        button2 = types.InlineKeyboardButton("Тех.поддержка", callback_data='tech_help')
+        markup.add(button1, button2)
+        bot.send_message(message.chat.id, text="Вот доступные тебе функции", reply_markup=markup)
+
+    # Добавляем кнопку "Другая кнопка" только для админов
+    elif role == 'admin':
+        # добавить подключение к другим базам данных, на роль админ
+        button2 = types.InlineKeyboardButton("Другая кнопка", callback_data='other_action')
+        markup.add(button1, button2)
+        bot.send_message(message.chat.id, text="Вот доступные тебе функции", reply_markup=markup)
+        print('Да, ты админ')
+
+    elif role =='developer':
+        button2 = types.InlineKeyboardButton("Что хочешь узнать?", callback_data='what_need')
+        markup.add(button1, button2)
+        bot.send_message(message.chat.id, text="Вот доступные тебе функции", reply_markup=markup)
+
+    else:
+        markup.add(button1)
+        bot.send_message(message.chat.id, text="Вот доступные тебе функции", reply_markup=markup)
+
+    conn.commit()
+    conn.close()
+
+#Вызов необходимых функций
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback(call):
+    #вызов функции анализ изображения
+    if call.data == 'analyze_image':
+        analis(call.message)
+    # вызов функции тех поддержки
+    elif call.data == 'tech_help':
+        tech(call.message)
+    # вызов функции другое
+    elif call.data == 'other_action':
+        admin(call.message)
+
+    # вызов функции для разработчика
+    elif call.data == 'what_need':
+        developer(call.message)
+
+def analis(message):
+    bot.send_message(message.chat.id, 'Вот твоё изображение')
+    # После выполнения возвращаем в меню
+    work(message)
+
+def tech(message):
+    bot.send_message(message.chat.id, 'Обращение в тех поддержку')
+    # После выполнения возвращаем в меню
+    work(message)
+
+def admin(message):
+    bot.send_message(message.chat.id, 'Вот, что я могу для тебя сделать')
+    work(message)
+
+def developer(message):
+    bot.send_message(message.chat.id, 'Доступ к каой базе данных нужен?')
+    work(message)
+
+    # user_states[message.chat.id] = PASSWORD
 
 bot.infinity_polling()
